623d61a6204b750a3d4cd0486d258643835fb309
Stanislav Chernyshev <madmasm@gmail.com>
2025-09-17 13:19:55 +0300

    fix

--- part_2/2.2/ex_2.dart
+++ part_2/2.2/ex_2.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 Stream<int> rawNumbers() => Stream.fromIterable([5, 2, 0, 4, 1]);
 
 void main() async {
-  // формируем поток, в котором все элементы исходного потока
-  // делятся на 0
+  // формируем поток, происходит целочисленное деление 10 с
+  // каждым элементом потока
   rawNumbers().map((n) => 10 ~/ n).listen(
     (v) => print('data: $v'),
     onError: (e) => print('error: $e'),
